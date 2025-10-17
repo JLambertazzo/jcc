2c7a66e89ba18762e48d951b302c895278f86333
Julien Bertazzo Lambert <jbertazzolambert@gmail.com>
2025-10-16 23:18:52 -0400

    wip: setup token types and match structure

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use regex::Regex;
 use std::fs;
+use std::vec;
 
 #[derive(Parser)]
 struct Cli {
@@ -13,13 +14,43 @@ struct Cli {
     filepath: String,
 }
 
-fn main() {
+enum TokenType {
+    Identifier,
+    Constant,
+    Keyword,
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenBrace,
+    CloseBrace,
+    Semicolon,
+}
+
+struct Token {
+    token_type: TokenType,
+    content: String,
+}
+
+fn classify_token(token_content: &String) -> Token {
+    /*
+     * Given regex patterns for each token type.
+     * Any other value you encounter at this point should be handled by exact match
+     */
+
+    let identifier_rgx = Regex::new(r"^[a-zA-Z_]\w*\b$").unwrap();
+    let constant_rgx = Regex::new(r"^[0-9]+\b$").unwrap();
+    let keyword_rgx = Regex::new(r"^(int|void|return)$").unwrap();
+
+    // TODO
+    return match (token_content) {};
+}
+
+fn lex_contents(src_contents: &String) -> Vec<Token> {
+    let tokens = Vec::new();
     let starting_whitespace_pattern = Regex::new(r"^\s+").unwrap();
     let next_token_pattern = Regex::new(r"^\w*\b").unwrap();
 
-    let cli = Cli::parse();
-    let mut contents =
-        fs::read_to_string(cli.filepath).expect("Should have been able to read the file");
+    let mut contents: String = src_contents.clone();
+
     while !contents.is_empty() {
         if starting_whitespace_pattern.is_match(&contents) {
             // trim starting whitespace
@@ -37,4 +68,12 @@ fn main() {
             let _token: String = contents.drain(rng).collect();
         }
     }
+    return tokens;
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let contents =
+        fs::read_to_string(cli.filepath).expect("Should have been able to read the file");
+    lex_contents(&contents);
 }

178298e6e4e2951c6aefadf3e4a3bc8e8190e416
Julien Bertazzo Lambert <jbertazzolambert@gmail.com>
2025-12-10 10:09:18 -0500

    feat: fix move instructions using two stack operands

--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -1,5 +1,6 @@
 mod add_stack_allocation;
 pub mod ast;
+mod fix_move_with_two_addresses;
 mod from_tacky;
 mod middleware;
 mod replace_pseudo;
@@ -12,6 +13,7 @@ pub fn tacky_program_to_asm_code(tacky_program: crate::tacky::ast::Program) -> S
         vec![
             replace_pseudo::replace_pseudoregisters_in_instructions,
             add_stack_allocation::add_stack_allocation_instruction,
+            fix_move_with_two_addresses::use_scratch_register_for_move,
         ],
     );
     to_code::asm_program_to_string(processed_asm_program)

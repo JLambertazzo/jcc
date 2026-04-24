9bf8313abb90d8a8997f7f5e3d9a3c792bfffeca
Julien BL <42924425+JLambertazzo@users.noreply.github.com>
2026-04-24 00:30:01 -0400

    feat: emit asm code for logical operators (#16)

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,5 @@
-LAST_COMPLETED_CHAPTER=3
-CHAPTER_IN_PROGRESS=4
+LAST_COMPLETED_CHAPTER=4
+# CHAPTER_IN_PROGRESS=4
 
-./writing-a-c-compiler-tests/test_compiler ./jcc --chapter $LAST_COMPLETED_CHAPTER --bitwise || { echo "Previous chapters' tests failed."; exit 1; }
-./writing-a-c-compiler-tests/test_compiler ./jcc --chapter $CHAPTER_IN_PROGRESS --bitwise --stage codegen
+./writing-a-c-compiler-tests/test_compiler ./jcc --chapter $LAST_COMPLETED_CHAPTER --bitwise # || { echo "Previous chapters' tests failed."; exit 1; }
+# ./writing-a-c-compiler-tests/test_compiler ./jcc --chapter $CHAPTER_IN_PROGRESS --bitwise --stage codegen
